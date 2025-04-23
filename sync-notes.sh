4d78f2b569d77c9b97d739d6fbc4e6dddd023c20
TristanEDU <tristanedu@protonmail.com>
2025-04-22 19:38:00 -0700

    Sync: 2025-04-22 19:38:00

--- sync-notes.sh
+++ sync-notes.sh
@@ -1,6 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
 cd ~/notes || exit
+
+# Update sidebar
+docsify-auto-sidebar --folder . --output ./_sidebar.md
 
 git add .
 git commit -m "Sync: $(date '+%Y-%m-%d %H:%M:%S')" 2>/dev/null
